4b2e3992f57bbd703725cf4f23e38d774c7759d8
dataluxe <phelan.carey@gmail.com>
2017-10-24 16:49:52 -0700

    Update solution.js

--- abbreviate-a-two-word-name/solution.js
+++ abbreviate-a-two-word-name/solution.js
@@ -1,1 +1,7 @@
-
+function abbrevName(name){
+    var out = '';
+    out += name.charAt(0).toUpperCase();
+    out += '.'
+    out += name.charAt(name.indexOf(" ") + 1).toUpperCase();
+    return out
+}
